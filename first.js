1107a35518ba900e0adf303a84f24faaf1c78d1e
Saurav Kumar <sauravkr582@gmail.com>
2025-06-05 08:56:36 +0530

    upi added

--- first.js
+++ first.js
@@ -4,5 +4,8 @@ console.log(button);
 const login = "Login page Added";
 console.log(login);
 
-const payment = "Integrated the payment gateway";
+const payment = "Integrated the gateway";
 console.log(payment);
+
+const UPI = "Integrated the upi ";
+console.log(UPI)
