416795d840d450220ac8be95d7e1863d9531f31e
Saurav Kumar <sauravkr582@gmail.com>
2025-06-05 08:53:06 +0530

    Payment

--- first.js
+++ first.js
@@ -2,4 +2,7 @@ const button = "Added a button ";
 console.log(button);
 
 const login = "Login page Added";
-console.log(login)
+console.log(login);
+
+const payment = "Integrated the payment gateway";
+console.log(payment);
